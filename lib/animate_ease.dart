6cb0396b34a9bddf22e6310d18a656d2f9ba7221
Usiobaifo Kenneth <kennethusiobaifo@yahoo.com>
2024-04-09 07:02:41 +0800

    all the class and animation logic completed

--- lib/animate_ease.dart
+++ lib/animate_ease.dart
@@ -1,7 +1,109 @@
+// This file defines the AnimateEase library which provides widget animation capabilities in Flutter apps.
+
 library animate_ease;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// Import statements to include Flutter material design widgets and custom components for animation.
+import 'package:animate_ease/components/animate_x.dart';
+import 'package:animate_ease/components/animation_type.dart';
+import 'package:animate_ease/components/visibility_detect.dart';
+import 'package:flutter/material.dart';
+
+/// The AnimateEase StatefulWidget allows for easy implementation of animations with various configurations.
+class AnimateEase extends StatefulWidget {
+  // Properties of the AnimateEase widget to customize the animation.
+  final Widget child; // The child widget to be animated.
+  final AnimationType animate; // The type of animation to apply.
+  final Duration duration; // Duration of the animation.
+  final Duration delay; // Delay before the animation starts.
+  final bool atRestAnimate; // Determines if the animation should play at rest.
+  final int? animationCount; // Nullable integer specifying the number of animation cycles.
+
+  // Constructor for AnimateEase with required and optional parameters, setting default values for optional ones.
+  /// usage 
+  /// - AnimateEase(child: any widget)
+  const AnimateEase({
+    super.key,
+    required this.child,
+    this.animate = AnimationType.fadeIn, // Default animation type is fadeIn.
+    this.duration = const Duration(seconds: 1), // Default duration of 1 second.
+    this.delay = const Duration(seconds: 0), // Default delay of 0 seconds.
+    this.atRestAnimate = true, // Default behavior to animate at rest.
+    this.animationCount, // Nullable, so it's optional and can be null.
+  });
+
+  @override
+  State<AnimateEase> createState() => _AnimateEaseState();
+}
+
+/// Private State class _AnimateEaseState handles the animation logic for AnimateEase widget.
+class _AnimateEaseState extends State<AnimateEase>
+    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
+  AnimationController? _controller; // Manages the animation.
+  Animation<double>? _animation; // The animation to be played.
+  int _animationCycleCount = 0; // Tracks the number of completed animation cycles.
+  late AnimationType _animationType; // Stores the type of animation to play.
+  final int _completedCycles = 0; // Counts the completed cycles, currently unused.
+  
+  @override
+  void initState() {
+    super.initState();
+    _animationType = widget.animate; // Initialize animation type from widget properties.
+    WidgetsBinding.instance.addObserver(this); // Add this class as an observer to lifecycle events.
+
+    // Initialize the animation controller with the duration and vsync provider.
+    _controller = AnimationController(duration: widget.duration, vsync: this)
+      ..addStatusListener((status) {
+        // Listen to the animation status to manage animation cycles.
+        if (status == AnimationStatus.completed) {
+          // Logic to handle repeat or reverse animation based on widget properties and cycle count.
+          if (!widget.atRestAnimate &&
+              (widget.animationCount == null ||
+                  _animationCycleCount < widget.animationCount! - 1)) {
+            _controller!.forward(from: 0.0);
+          } else if (widget.atRestAnimate &&
+              widget.animationCount != null &&
+              _animationCycleCount < widget.animationCount!) {
+            _controller!.reverse(from: 1.0);
+          }
+          _animationCycleCount++;
+        } else if (status == AnimationStatus.dismissed) {
+          if (widget.atRestAnimate &&
+              (widget.animationCount == null ||
+                  _animationCycleCount < widget.animationCount!)) {
+            _controller!.forward();
+          }
+        }
+      });
+
+    // Call setup to initialize the animation based on the type and controller.
+    AnimateX.setup(_animation, _controller, _animationType);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // Lifecycle method to resume animation when the app comes to the foreground.
+    if (state == AppLifecycleState.resumed) {
+      _controller?.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Builds the widget tree, incorporating visibility detection and animation.
+    return VisibilityDetect.visib(
+      widget, 
+      _animation, 
+      _controller, 
+      _completedCycles, 
+      _animationType,
+    );
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the animation controller and remove the lifecycle observer on widget disposal.
+    _controller?.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 }
---
+++ lib/components/visibility_detect.dart
@@ -0,0 +1,473 @@
+// Import necessary packages for defining custom animations and detecting widget visibility.
+import 'package:animate_ease/components/animation_type.dart';
+import 'package:flutter/widgets.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+/// The VisibilityDetect class provides a static method to dynamically handle the visibility of widgets
+/// and apply animations based on the AnimationType specified. It utilizes the VisibilityDetector package
+/// to trigger animations when widgets become visible on the screen.
+class VisibilityDetect {
+  /// The visib static method creates a VisibilityDetector that listens for visibility changes of the widget
+  /// and applies animations accordingly.
+  ///
+  /// - Parameters:
+  ///   - widget: The widget to which the visibility detection and animation should be applied.
+  ///   - animation: The animation that will be applied to the widget. This should be a reference to an Animation<double> object.
+  ///   - controller: The AnimationController that controls the animation.
+  ///   - completedCycles: The number of completed animation cycles.
+  ///   - animationType: The type of animation to apply, as defined in AnimationType.
+  ///
+  /// - Returns: A VisibilityDetector widget that wraps the input widget and applies the specified animation when the widget becomes visible.
+  static visib(
+    dynamic widget,
+    Animation<double>? animation,
+    AnimationController? controller,
+    int completedCycles,
+    AnimationType? animationType,
+  ) {
+    return VisibilityDetector(
+      key: Key('KSwagger_${widget.animate.toString()}'), // Unique key for the detector based on the animation type.
+      onVisibilityChanged: (visibilityInfo) {
+        var visiblePercentage = visibilityInfo.visibleFraction * 100; // Calculate the percentage of the widget that is visible.
+        if (visiblePercentage > 0) { // If any part of the widget is visible,
+          if (!controller.isAnimating && // and the animation is not currently running,
+              (controller.status != AnimationStatus.completed || // and the animation hasn't completed or needs to repeat,
+                  widget.atRestAnimate)) {
+            // If animationCount is not set or completedCycles is less than the animationCount, start the animation.
+            if (widget.animationCount == null || completedCycles < widget.animationCount!) {
+              Future.delayed(widget.delay, () => controller.forward(from: 0.0)); // Start the animation after the specified delay.
+            }
+          }
+        }
+      },
+      child: AnimatedBuilder(
+        animation: controller!, // The animation controller that drives the animation.
+        builder: (context, child) {
+          switch (animationType) { // Switch on the type of animation to apply different transformations.
+            case AnimationType.fadeIn:
+              return Opacity(
+                opacity: animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.fadeOut:
+              return Opacity(
+                opacity: animation!.value,
+                child: widget.child,
+              );
+            case AnimationType.rotate:
+              return Transform.rotate(
+                angle: animation!.value * 2 * 3.141592653589793,
+                child: widget.child,
+              );
+            case AnimationType.scaleUp:
+              return Transform.scale(
+                scale: 1.0 - animation!.value,
+                child: widget.child,
+              );
+            case AnimationType.scaleDown:
+              return Transform.scale(
+                scale: animation!.value,
+                child: widget.child,
+              );
+            case AnimationType.slideInRight:
+              return Transform.translate(
+                offset: Offset(
+                    animation!.value * MediaQuery.of(context).size.width, 0.0),
+                child: widget.child,
+              );
+
+            case AnimationType.slideOutLeft:
+              return Transform.translate(
+                offset: Offset(
+                    -animation!.value * MediaQuery.of(context).size.width, 0.0),
+                child: widget.child,
+              );
+
+            case AnimationType.slideInBottom:
+              return Transform.translate(
+                offset: Offset(
+                    0.0, animation!.value * MediaQuery.of(context).size.height),
+                child: widget.child,
+              );
+            case AnimationType.slideOutTop:
+              return Transform.translate(
+                offset: Offset(0.0,
+                    -animation!.value * MediaQuery.of(context).size.height),
+                child: widget.child,
+              );
+            case AnimationType.slideInTop:
+              return Transform.translate(
+                offset: Offset(
+                    0.0, animation!.value * MediaQuery.of(context).size.height),
+                child: widget.child,
+              );
+            case AnimationType.slideOutRight:
+              return Transform.translate(
+                offset: Offset(
+                    animation!.value * MediaQuery.of(context).size.width, 0.0),
+                child: widget.child,
+              );
+            case AnimationType.slideInLeft:
+              return Transform.translate(
+                offset: Offset(
+                    -animation!.value * MediaQuery.of(context).size.width, 0.0),
+                child: widget.child,
+              );
+
+            case AnimationType.slideOutBottom:
+              return Transform.translate(
+                offset: Offset(0.0,
+                    -animation!.value * MediaQuery.of(context).size.height),
+                child: widget.child,
+              );
+            case AnimationType.rotateIn:
+              return Transform.rotate(
+                angle: animation!.value * (3.141592653589793 / 2),
+                child: widget.child,
+              );
+            case AnimationType.rotateOut:
+              return Transform.rotate(
+                angle: -animation!.value * (3.141592653589793 / 2),
+                child: widget.child,
+              );
+            case AnimationType.opacityInLeft:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                      -(animation.value * MediaQuery.of(context).size.width),
+                      0.0),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.opacityInRight:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                      animation.value * MediaQuery.of(context).size.width, 0.0),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.opacityInUp:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(0.0,
+                      -(animation.value * MediaQuery.of(context).size.height)),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.opacityInDown:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(0.0,
+                      animation.value * MediaQuery.of(context).size.height),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.translateInLeft:
+              return Transform.translate(
+                offset: Offset(
+                    -(animation!.value * MediaQuery.of(context).size.width),
+                    0.0),
+                child: widget.child,
+              );
+            case AnimationType.translateInRight:
+              return Transform.translate(
+                offset: Offset(
+                    animation!.value * MediaQuery.of(context).size.width, 0.0),
+                child: widget.child,
+              );
+            case AnimationType.translateInUp:
+              return Transform.translate(
+                offset: Offset(0.0,
+                    -(animation!.value * MediaQuery.of(context).size.height)),
+                child: widget.child,
+              );
+            case AnimationType.translateInDown:
+              return Transform.translate(
+                offset: Offset(
+                  animationType == AnimationType.translateInLeft ||
+                          animationType == AnimationType.translateInRight
+                      ? animation!.value * MediaQuery.of(context).size.width
+                      : 0.0,
+                  animationType == AnimationType.translateInUp ||
+                          animationType == AnimationType.translateInDown
+                      ? animation!.value * MediaQuery.of(context).size.height
+                      : 0.0,
+                ),
+                child: widget.child,
+              );
+            case AnimationType.scatteredIn:
+              return Transform(
+                transform: Matrix4.diagonal3Values(
+                    animation!.value, animation.value, 1.0),
+                child: widget.child,
+              );
+            case AnimationType.particles:
+              return Transform(
+                transform: Matrix4.translationValues(
+                    animation!.value * 10, animation.value * 10, 0.0),
+                child: widget.child,
+              );
+            case AnimationType.rainFall:
+              return Transform.translate(
+                offset: Offset(
+                    0.0, animation!.value * MediaQuery.of(context).size.height),
+                child: widget.child,
+              );
+            case AnimationType.rollInLeft:
+              return Transform.rotate(
+                angle: animation!.value * (3.141592653589793 / 2),
+                child: Transform.translate(
+                  offset: Offset(
+                      -(animation.value * MediaQuery.of(context).size.width),
+                      0.0),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.rollInRight:
+              return Transform.rotate(
+                angle: animation!.value * (-3.141592653589793 / 2),
+                child: Transform.translate(
+                  offset: Offset(
+                      animation.value * MediaQuery.of(context).size.width, 0.0),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.rollInUp:
+              return Transform.rotate(
+                angle: animation!.value * (-3.141592653589793 / 2),
+                child: Transform.translate(
+                  offset: Offset(0.0,
+                      -(animation.value * MediaQuery.of(context).size.height)),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.rollInDown:
+              return Transform.rotate(
+                angle: animation!.value * (3.141592653589793 / 2),
+                child: Transform.translate(
+                  offset: Offset(0.0,
+                      animation.value * MediaQuery.of(context).size.height),
+                  child: widget.child,
+                ),
+              );
+            case AnimationType.bounceIn:
+              return Transform.scale(
+                scale: 1 - animation!.value.abs(),
+                child: widget.child,
+              );
+
+            case AnimationType.bounceOut:
+              return Transform.scale(
+                scale: animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.elasticIn:
+              return Transform.scale(
+                scale: 1 - animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.elasticOut:
+              return Transform.scale(
+                scale: animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.jitter:
+              return Transform(
+                transform: Matrix4.translationValues(
+                  animation!.value * 5,
+                  animation.value * 5,
+                  0.0,
+                ),
+                child: widget.child,
+              );
+
+            case AnimationType.wiggle:
+              return Transform(
+                transform: Matrix4.rotationZ(
+                  animation!.value * 0.1,
+                ),
+                child: widget.child,
+              );
+
+            case AnimationType.pulse:
+              return Transform.scale(
+                scale: 1 - (animation!.value * 0.2),
+                child: widget.child,
+              );
+
+            case AnimationType.expand:
+              return Transform.scale(
+                scale: animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.collapse:
+              return Transform.scale(
+                scale: 1 - animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.shake:
+              return Transform(
+                transform: Matrix4.translationValues(
+                  animation!.value * 10,
+                  0.0,
+                  0.0,
+                ),
+                child: widget.child,
+              );
+
+            case AnimationType.swing:
+              return Transform.rotate(
+                angle: animation!.value * 0.2,
+                child: widget.child,
+              );
+
+            case AnimationType.tada:
+              return Transform.scale(
+                scale: 1 + animation!.value * 0.1,
+                child: widget.child,
+              );
+
+            case AnimationType.wobble:
+              return Transform(
+                transform: Matrix4.rotationZ(
+                  animation!.value * 0.2,
+                ),
+                child: widget.child,
+              );
+
+            case AnimationType.jello:
+              return Transform(
+                transform: Matrix4.skewX(
+                  animation!.value * 0.2,
+                ),
+                child: widget.child,
+              );
+
+            case AnimationType.heartBeat:
+              return Transform.scale(
+                scale: 1 - animation!.value.abs() * 0.2,
+                child: widget.child,
+              );
+
+            case AnimationType.flash:
+              return Opacity(
+                opacity: animation!.value < 0.5 ? 1.0 : 0.0,
+                child: widget.child,
+              );
+
+            case AnimationType.rubberBand:
+              return Transform.scale(
+                scale: 1 - animation!.value.abs() * 0.3,
+                child: widget.child,
+              );
+
+            case AnimationType.headShake:
+              return Transform.rotate(
+                angle: animation!.value * 0.2,
+                child: widget.child,
+              );
+
+            case AnimationType.squeeze:
+              return Transform.scale(
+                scale: animation!.value < 0.5
+                    ? 1 - animation.value
+                    : animation.value,
+                child: widget.child,
+              );
+            case AnimationType.flipX:
+              return Transform(
+                transform: Matrix4.rotationY(
+                  animation!.value * 3.141592653589793,
+                ),
+                alignment: Alignment.center,
+                child: widget.child,
+              );
+
+            case AnimationType.flipY:
+              return Transform(
+                transform: Matrix4.rotationX(
+                  animation!.value * 3.141592653589793,
+                ),
+                alignment: Alignment.center,
+                child: widget.child,
+              );
+
+            case AnimationType.zoomIn:
+              return Transform.scale(
+                scale: animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.zoomOut:
+              return Transform.scale(
+                scale: 1 - animation!.value,
+                child: widget.child,
+              );
+
+            case AnimationType.slideInLeftFade:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                    -animation.value * MediaQuery.of(context).size.width,
+                    0.0,
+                  ),
+                  child: widget.child,
+                ),
+              );
+
+            case AnimationType.slideInRightFade:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                    animation.value * MediaQuery.of(context).size.width,
+                    0.0,
+                  ),
+                  child: widget.child,
+                ),
+              );
+
+            case AnimationType.slideInTopFade:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                    0.0,
+                    -animation.value * MediaQuery.of(context).size.height,
+                  ),
+                  child: widget.child,
+                ),
+              );
+
+            case AnimationType.slideInBottomFade:
+              return Opacity(
+                opacity: animation!.value,
+                child: Transform.translate(
+                  offset: Offset(
+                    0.0,
+                    animation.value * MediaQuery.of(context).size.height,
+                  ),
+                  child: widget.child,
+                ),
+              );
+
+            default:
+              return widget.child;
+          }
+        },
+      ),
+    );
+  }
+}
